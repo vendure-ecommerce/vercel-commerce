1aacccd7555b9353457bb02cfc649147c9517728
David Höck <d.hoeck@alpin11.com>
2025-01-22 11:22:48 +0100

    fix: channel context types

--- components/cart/channel-context.tsx
+++ components/cart/channel-context.tsx
@@ -1,17 +1,15 @@
 'use client';
 
 import { createContext, PropsWithChildren, use, useContext } from 'react';
-import { GetActiveChannelQuery } from '../../lib/vendure/types';
+import { Active_ChannelFragment } from '@/lib/vendure/types';
 
-type ActiveCannel = Pick<GetActiveChannelQuery, 'activeChannel'>
-
-const ChannelContext = createContext<ActiveCannel | undefined>(undefined);
+const ChannelContext = createContext<Active_ChannelFragment | undefined>(undefined);
 
 export function ChannelProvider({
   channelPromise,
   children
 }: PropsWithChildren<{
-  channelPromise: Promise<ActiveCannel | undefined>;
+  channelPromise: Promise<Active_ChannelFragment | undefined>;
 }>) {
   const channel = use(channelPromise);
 
